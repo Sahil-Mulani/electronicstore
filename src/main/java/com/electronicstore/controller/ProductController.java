cfc653298d4cdf81f960dc8a6032c831f499fbc1
Sahil-Mulani <sahilmulani646@gmail.com>
2023-12-18 14:32:29 +0530

    apply changes in user controller

--- src/main/java/com/electronicstore/controller/ProductController.java
+++ src/main/java/com/electronicstore/controller/ProductController.java
@@ -37,6 +37,12 @@ public class ProductController {
     @Autowired
     private FileServiceI fileServiceI;
 
+    /**
+     * @auther sahil
+     * @param productDto
+     * @return
+     */
+
     @PostMapping("/")
     public ResponseEntity<ProductDto> createProduct(@Valid @RequestBody ProductDto productDto) {
         log.info("Entering product for create product");
@@ -46,19 +52,29 @@ public class ProductController {
     }
 
     /**
-     *
+     * @auther sahil
+     * @apiNote for get single product
      * @param productId
      * @return
      */
     @GetMapping("/{productId}")
     public ResponseEntity<ProductDto> getSingleProduct(@PathVariable String productId) {
+        log.info("Entering product for get single product");
+
         ProductDto singleProduct = this.productService.getSingleProduct(productId);
+        log.info("complete product for get single prod");
         return new ResponseEntity<>(singleProduct, HttpStatus.OK);
     }
 
+    /**
+     *
+     * @param productId
+     * @return
+     */
+
     @DeleteMapping("/{productId}")
     public ResponseEntity<ApiResponse> deleteById(@PathVariable String productId) {
-
+        log.info("Entering product for delete product by using Id ");
         ApiResponse apiResponse = ApiResponse.builder().message(AppConstant.DELETE).status(true).statusCode(HttpStatus.OK).build();
         this.productService.deleteProduct(productId);
 
@@ -66,12 +82,29 @@ public class ProductController {
 
     }
 
+    /**
+     *
+     * @param productId
+     * @param dto
+     * @return
+     */
+
     @PutMapping("/{productId}")
     public ResponseEntity<ProductDto> updateProduct(@Valid @PathVariable String productId, @RequestBody ProductDto dto) {
-
+        log.info("Eneting product for update the product");
         ProductDto productDto = this.productService.updateProduct(productId, dto);
+        log.info("Complete product for update the product");
         return new ResponseEntity<>(productDto, HttpStatus.OK);
     }
+
+    /**
+     *
+     * @param pageNumber
+     * @param pageSize
+     * @param sortBy
+     * @param direction
+     * @return
+     */
 
     @GetMapping("/")
     public ResponseEntity<PageableResponse> getAllProducts(
@@ -81,11 +114,21 @@ public class ProductController {
             @RequestParam(value = "direction", defaultValue = AppConstant.SORT_DIR, required = false) String direction
 
     ) {
-
+           log.info("Entering product for geting all product data");
         PageableResponse<ProductDto> allProducts = this.productService.getAllProducts(pageNumber, pageSize, sortBy, direction);
+        log.info("Complete product for geting all product data");
         return new ResponseEntity<>(allProducts, HttpStatus.OK);
 
     }
+
+    /**
+     *
+     * @param pageNumber
+     * @param pageSize
+     * @param sortBy
+     * @param direction
+     * @return
+     */
 
     @GetMapping("/trueLiveProducts")
     public ResponseEntity<PageableResponse> findAllLiveTrueProducts(
@@ -94,10 +137,20 @@ public class ProductController {
             @RequestParam(value = "sortBy", defaultValue = AppConstant.PRODUCT_SORT_BY, required = false) String sortBy,
             @RequestParam(value = "direction", defaultValue = AppConstant.SORT_DIR, required = false) String direction
     ) {
-
+        log.info("Enteering product for find all live true product data");
         PageableResponse<ProductDto> allLIveProducts = this.productService.findByliveTrue(pageNumber, pageSize, sortBy, direction);
+        log.info("Complete product for find all live true product data");
         return new ResponseEntity<>(allLIveProducts, HttpStatus.OK);
     }
+
+    /**
+     *
+     * @param pageNumber
+     * @param pageSize
+     * @param sortBy
+     * @param direction
+     * @return
+     */
 
     @GetMapping("/liveProducts")
     public ResponseEntity<PageableResponse> getLiveProducts(
@@ -106,11 +159,23 @@ public class ProductController {
             @RequestParam(value = "sortBy", defaultValue = AppConstant.PRODUCT_SORT_BY, required = false) String sortBy,
             @RequestParam(value = "direction", defaultValue = AppConstant.SORT_DIR, required = false) String direction
     ) {
+        log.info("Entering product for get livee products data");
 
         PageableResponse<ProductDto> allLIveProducts = this.productService.getAllLIveProducts(pageNumber, pageSize, sortBy, direction);
+        log.info("Entering product for get live products data");
         return new ResponseEntity<>(allLIveProducts, HttpStatus.OK);
 
     }
+
+    /**
+     *
+     * @param pageNumber
+     * @param pageSize
+     * @param sortBy
+     * @param direction
+     * @param keyword
+     * @return
+     */
 
     @GetMapping("/keyword/{keyword}")
     public ResponseEntity<PageableResponse> getProductByTitle(
@@ -120,15 +185,24 @@ public class ProductController {
             @RequestParam(value = "direction", defaultValue = AppConstant.SORT_DIR, required = false) String direction
             , @PathVariable String keyword) {
 
+        log.info("Enetring product for get product by title ");
         PageableResponse<ProductDto> productByTitle = this.productService.getProductByTitle(keyword, pageNumber, pageSize, sortBy, direction);
-
+        log.info("Complete pproduct for get product by title ");
         return new ResponseEntity<>(productByTitle, HttpStatus.OK);
     }
+
+    /**
+     *
+     * @param image
+     * @param productId
+     * @return
+     * @throws IOException
+     */
 
 
     @PostMapping("/image/{productId}")
     public ResponseEntity<ImageResponse> uploadImage(@RequestParam MultipartFile image, @PathVariable String productId) throws IOException {
-
+        log.info("Entering product for upload image ");
         String file = this.fileServiceI.uploadFile(image, path);
 
         ProductDto product = this.productService.getSingleProduct(productId);
@@ -138,19 +212,27 @@ public class ProductController {
         ProductDto updatedProduct = this.productService.updateProduct(productId, product);
 
         ImageResponse imageResponse = ImageResponse.builder().message("Image Uploaded").imageName(file).status(true).statusCode(HttpStatus.CREATED).build();
-
+        log.info("compltete product for uploading image");
         return new ResponseEntity<>(imageResponse, HttpStatus.CREATED);
 
     }
+
+    /**
+     *
+     * @param categoryId
+     * @param response
+     * @throws IOException
+     */
 
 
     @GetMapping("/image/{categoryId}")
     public void getProductImage(@PathVariable String categoryId, HttpServletResponse response) throws IOException {
 
-
+        log.info("Entering product for get product by Image");
         ProductDto product = this.productService.getSingleProduct(categoryId);
         InputStream resource = this.fileServiceI.getResource(path, product.getImage());
         response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        log.info("Complete product for get product by image");
         StreamUtils.copy(resource, response.getOutputStream());
     }
 
