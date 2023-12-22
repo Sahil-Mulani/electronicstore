9eab4b014772ee304e3648e8423f00e2ad089b2a
Sahil-Mulani <sahilmulani646@gmail.com>
2023-12-22 19:20:36 +0530

    Create Order and OrderItem Entity

---
+++ src/main/java/com/electronicstore/Enitity/Order.java
@@ -0,0 +1,48 @@
+package com.electronicstore.Enitity;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    private  String orderId;
+    //pending,Delivered
+    private String orderStatus;
+    //NotPaid,Paid
+    private String paymentStatus;
+
+    private int orderAmount;
+
+    private String billingAddress;
+
+    private String billingPhone;
+
+    private String billingName;
+
+    private Date orderDate;
+
+    private Date deliveredDate;
+
+    //user
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "order",fetch = FetchType.EAGER)
+    private List<OrderItem> orderItems=new ArrayList<>();
+
+
+}
