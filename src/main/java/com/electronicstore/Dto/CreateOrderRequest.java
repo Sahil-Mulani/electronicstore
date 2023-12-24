1a002d788bd40e4a0d94953331d10d6de3d0d318
Sahil-Mulani <sahilmulani646@gmail.com>
2023-12-24 19:02:59 +0530

    Create OrderDto Request

---
+++ src/main/java/com/electronicstore/Dto/CreateOrderRequest.java
@@ -0,0 +1,30 @@
+package com.electronicstore.Dto;
+
+
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
+@ToString
+public class CreateOrderRequest {
+
+    @NotBlank(message = "Cart id is required")
+    private String cartId;
+    @NotBlank(message = "user id is required")
+    private String id;
+    private String orderStatus="PENDING";
+    private String paymentStatus="NOTPAID";
+    @NotBlank(message = "Address is required")
+    private String billingAddress;
+    @NotBlank(message = "Phone number is required")
+    private String billingPhone;
+    @NotBlank(message = "Billing Name is required")
+    private String billingName;
+}
